ae3632c7553fbc958a05a4999e8cf929dc440505
bobvhfhh <152941272+bobvhfhh@users.noreply.github.com>
2025-03-05 21:45:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 
 let mytoken = 'auto';
 let guestToken = ''; //可以随便取，或者uuid生成，https://1024tools.com/uuid
-let BotToken = '8034673917:AAEAWMYrNuBm8QqBRMQNfMALwfUqeRJNBp0''; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
+let BotToken = '7631830745:AAE0WH-1mT5PUv6fUpj146daR9bQZWuTaoA'; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
 let ChatID = '6401271317'; //可以为空，或者@userinfobot中获取，/start
 let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
 let FileName = 'CF-Workers-SUB';
